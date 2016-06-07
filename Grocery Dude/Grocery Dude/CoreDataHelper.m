2e0c25c4e1da7521aadb81b74ccd7b9f609f20cb
zlk <zlk@meitu.com>
2016-06-07 20:01:59 +0800

    【自测】完成CoreDataHelper类

--- Grocery Dude/Grocery Dude/CoreDataHelper.m
+++ Grocery Dude/Grocery Dude/CoreDataHelper.m
@@ -16,6 +16,8 @@
 NSString *storeFilename = @"Grocery-Dude.sqlite";
 
 #pragma mark - PATHS
+
+// 返回沙盒的Document目录
 - (NSString *)applicationDocumentDirectory {
     
     if (debug == 1) {
@@ -25,6 +27,7 @@ NSString *storeFilename = @"Grocery-Dude.sqlite";
     
 }
 
+// 在Documents目录下新建Stores目录
 - (NSURL *)applicationStoreDirectory {
     
     if (debug == 1) {
@@ -52,6 +55,101 @@ NSString *storeFilename = @"Grocery-Dude.sqlite";
     }
     
     return storesDirectory;
+    
+}
+
+// 返回数据库文件的path url
+- (NSURL *)storeURL {
+    
+    if (debug == 1) {
+        NSLog(@"Running %@ '%@'", self.class, NSStringFromSelector(_cmd));
+    }
+    
+    return [[self applicationStoreDirectory] URLByAppendingPathComponent:storeFilename];
+    
+}
+
+#pragma mark - SETUP
+
+- (id)init {
+    
+    if (debug == 1) {
+        NSLog(@"Running %@ '%@'", self.class, NSStringFromSelector(_cmd));
+    }
+    
+    self = [super init];
+    if (!self) {
+        return nil;
+    }
+    
+    _model = [NSManagedObjectModel mergedModelFromBundles:nil];
+    _coordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:_model];
+    _context = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSMainQueueConcurrencyType];
+    [_context setPersistentStoreCoordinator:_coordinator];
+    
+    return self;
+}
+
+
+- (void)loadStore {
+    
+    if (debug == 1) {
+        NSLog(@"Running %@ '%@'", self.class, NSStringFromSelector(_cmd));
+    }
+    
+    if (_store) {
+        // 如果已经存在，就直接return
+        return;
+    }
+    
+    NSError *error = nil;
+    
+    _store = [_coordinator addPersistentStoreWithType:NSSQLiteStoreType
+                                        configuration:nil
+                                                  URL:[self storeURL]
+                                              options:nil
+                                                error:&error];
+    if (!_store) {
+        NSLog(@"Failed to Add store. Error: %@", error);
+        abort();
+    } else {
+        if (debug == 1) {
+            NSLog(@"Successfully added store: %@", _store);
+        }
+    }
+    
+}
+
+- (void)setupCoreData {
+    
+    if (debug == 1) {
+        NSLog(@"Running %@ '%@'", self.class, NSStringFromSelector(_cmd));
+    }
+    
+    [self loadStore];
+    
+    
+}
+
+#pragma mark - SAVING
+
+- (void)saveContext {
+    
+    if (debug == 1) {
+        NSLog(@"Running %@ '%@'", self.class, NSStringFromSelector(_cmd));
+    }
+    
+    if ([_context hasChanges]) {
+        NSError *error = nil;
+        if ([_context save:&error]) {
+            NSLog(@"_context SAVED changes to persistent store");
+        } else {
+            NSLog(@"Failed to save _context: %@", error);
+        }
+        
+    } else {
+        NSLog(@"SKIPPED _context save, there are no changes!");
+    }
     
 }
 
